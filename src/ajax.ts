0ea2d2908cba258f0df6e425d016e00bb11783c5
nickBarak <nicholasjbarak@gmail.com>
2021-01-19 08:07:51 -0800

    readying for merge with view-information

--- src/ajax.ts
+++ src/ajax.ts
@@ -1,6 +1,5 @@
 import { axiosInstance } from './util/axiosConfig';
 import { Batch } from './types';
-import axios from 'axios';
 
 /**
  * This function gets all of the batch data from our back end. This
@@ -8,11 +7,7 @@ import axios from 'axios';
  */
 export const getBatchData = async (batchId: string|number) : Promise<Batch|undefined> => {
     try {
-        console.log('getting batch data...');
-        // const response = await (await axiosInstance())?.get<Batch>("/client/batch/" + batchId);
-        const response = await axios.get<Batch>('http://localhost:9011' + "/client/batch/" + batchId);
-        console.log('batch data received:');
-        response?.data ? console.log(response.data) : console.log('no data');
+        const response = await (await axiosInstance())?.get<Batch>("/client/batch/" + batchId);
         if (response?.data) return response.data;
     } catch (error:any) {
         console.log(error);
